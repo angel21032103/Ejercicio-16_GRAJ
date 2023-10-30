58a50e2df5b6bc1e25dbe993b4b648a849446d6a
null <null>
2023-10-30 12:26:03 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Ejercicio_16_GRAJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtpalabra = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbRespuesta = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(561, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Programa especializado en identificar palindromos";
+            // 
+            // txtpalabra
+            // 
+            this.txtpalabra.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpalabra.Location = new System.Drawing.Point(271, 138);
+            this.txtpalabra.Name = "txtpalabra";
+            this.txtpalabra.Size = new System.Drawing.Size(282, 25);
+            this.txtpalabra.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingresa la palabra ";
+            // 
+            // lbRespuesta
+            // 
+            this.lbRespuesta.AutoSize = true;
+            this.lbRespuesta.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRespuesta.Location = new System.Drawing.Point(290, 203);
+            this.lbRespuesta.Name = "lbRespuesta";
+            this.lbRespuesta.Size = new System.Drawing.Size(0, 24);
+            this.lbRespuesta.TabIndex = 3;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnVerificar.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(586, 140);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(86, 23);
+            this.btnVerificar.TabIndex = 4;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(773, 270);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lbRespuesta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtpalabra);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtpalabra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbRespuesta;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Ejercicio_16_GRAJ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            string palabra = txtpalabra.Text;
+
+            if (EsPalindromo(palabra))
+            {
+                lbRespuesta.Text = "La frase es un palíndromo.";
+            }
+            else
+            {
+                lbRespuesta.Text = "La frase no es un palíndromo.";
+            }
+        }
+
+
+        private bool EsPalindromo(string palabra)
+        {
+            Stack<char> pila = new Stack<char>();
+            Queue<char> cola = new Queue<char>();
+
+            palabra = palabra.Replace(" ", "").ToLower();
+
+            foreach (char letra in palabra)
+            {
+                pila.Push(letra);
+                cola.Enqueue(letra);
+            }
+
+         
+            while (pila.Count > 0 && cola.Count > 0)
+            {
+                if (pila.Pop() != cola.Dequeue())
+                {
+                    return false; // No es un palíndromo
+                }
+            }
+
+            return true; // Es un palíndromo
+        }
+    }
+}
